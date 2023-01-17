d0cadb80c34bc3d4e9cc16366fb6c4f0e5579504
vasilevasluian <vasilevasluian@gmail.com>
2023-01-17 21:45:27 +0200

    Added footer block and created methods for validation

--- src/test/java/com/sauce/demo/gui/BasePage.java
+++ src/test/java/com/sauce/demo/gui/BasePage.java
@@ -1,8 +1,10 @@
 package com.sauce.demo.gui;
 
+import com.sauce.demo.gui.pages.blocks.Footer;
 import com.sauce.demo.gui.pages.blocks.Header;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.assertj.core.api.SoftAssertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -15,4 +17,38 @@ public class BasePage {
 
     @Autowired
     Header header;
+
+    @Autowired
+    Footer footer;
+
+    public void validateFooter(){
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(getFooter().getFooterData().isPresent())
+                    .describedAs("Footer data is displayed")
+                    .isTrue();
+            softly.assertThat(getFooter().getLinkedinButton().isPresent())
+                    .describedAs("LinkedIn Button is displayed")
+                    .isTrue();
+            softly.assertThat(getFooter().getFacebookButton().isPresent())
+                    .describedAs("Facebook Button is displayed")
+                    .isTrue();
+            softly.assertThat(getFooter().getTwitterButton().isPresent())
+                    .describedAs("Twitter Button is displayed")
+                    .isTrue();
+        });
+    }
+
+    public void validateHeader(){
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(getHeader().getAppLogo().isPresent())
+                    .describedAs("App logo is displayed")
+                    .isTrue();
+            softly.assertThat(getHeader().getShoppingCart().isPresent())
+                    .describedAs("Shopping cart is displayed")
+                    .isTrue();
+            softly.assertThat(getHeader().getMenuButton().isPresent())
+                    .describedAs("Menu button is displayed")
+                    .isTrue();
+        });
+    }
 }
